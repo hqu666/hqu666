e2a4923d562b8eddca53b0c4284709b8738f159f
hiroomi kuwayama <hqu666@gmail.com>
2017-08-16 00:08:43 +0900

    filelist2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+			this.components = new System.ComponentModel.Container();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.timeNow = new System.Windows.Forms.Label();
+			this.secTimer = new System.Windows.Forms.Timer(this.components);
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.fileinfo = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Location = new System.Drawing.Point(444, 33);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(596, 512);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(12, 33);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(426, 148);
+			this.listBox1.TabIndex = 2;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// timeNow
+			// 
+			this.timeNow.AutoSize = true;
+			this.timeNow.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.timeNow.Location = new System.Drawing.Point(12, 521);
+			this.timeNow.Name = "timeNow";
+			this.timeNow.Size = new System.Drawing.Size(161, 24);
+			this.timeNow.TabIndex = 4;
+			this.timeNow.Text = "12時34分 56秒";
+			this.timeNow.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.timeNow.UseMnemonic = false;
+			// 
+			// secTimer
+			// 
+			this.secTimer.Enabled = true;
+			this.secTimer.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(98, 4);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.comboBox1.Size = new System.Drawing.Size(61, 23);
+			this.comboBox1.TabIndex = 5;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(12, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(80, 15);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "ドライブ選択";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(12, 187);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(426, 280);
+			this.treeView1.TabIndex = 7;
+			this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+			// 
+			// fileinfo
+			// 
+			this.fileinfo.AutoSize = true;
+			this.fileinfo.Location = new System.Drawing.Point(190, 4);
+			this.fileinfo.MinimumSize = new System.Drawing.Size(850, 0);
+			this.fileinfo.Name = "fileinfo";
+			this.fileinfo.Size = new System.Drawing.Size(850, 12);
+			this.fileinfo.TabIndex = 8;
+			this.fileinfo.Text = "ファイル情報";
+			this.fileinfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 473);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(426, 19);
+			this.textBox1.TabIndex = 9;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 499);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(426, 19);
+			this.textBox2.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1039, 550);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.fileinfo);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.timeNow);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "Form1";
+			this.Text = "これから始まる";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
         }
 
         #endregion
-    }
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label timeNow;
+        private System.Windows.Forms.Timer secTimer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label fileinfo;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+	}
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,249 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		DirectoryInfo di;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			makeDriveList();
+		}
+
+		private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			TreeNode tn = e.Node, tn2;
+			string d = tn.FullPath;//展開するノードのフルパスを取得
+			tn.Nodes.Clear();
+			di = new DirectoryInfo( d );//ディレクトリ一覧を取得
+			try {
+				foreach (DirectoryInfo d2 in di.GetDirectories()) { 
+					tn2 = new TreeNode( d2.Name, 1, 2 );
+					tn.Nodes.Add( tn2 );
+					folderItemListUp( d2.Name,tn2 );
+				//	tn2.Nodes.Add( "..." );
+				}
+				foreach (FileInfo fi in di.GetFiles()) {
+					tn2 = new TreeNode( fi.Name, 3, 3 );
+					tn.Nodes.Add( tn2 );
+				}
+			} catch { }
+		}       //ノードを展開しようとしているときに発生するイベント
+
+		private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)//NodeMouseClickが利かなかった
+		{
+			string selectItem = e.Node.Text;// = listBox1.SelectedItem.ToString();
+
+		//	string selectItem = listBox1.SelectedItem.ToString();
+			FileInfo fi = new FileInfo( selectItem );
+			String infoStr = ",Exists;";
+			infoStr += fi.Exists;
+			infoStr += ",拡張子;";
+			infoStr += fi.Extension;
+			infoStr += "作成;";
+			infoStr += fi.CreationTime;
+			infoStr += ",アクセス;";
+			infoStr += fi.LastAccessTime;
+			infoStr += ",更新;";
+			infoStr += fi.LastWriteTime;
+			if (fi.Exists) {
+				infoStr += ",ファイルサイズ;";
+				infoStr += fi.Length;
+			} else {
+				TreeNode tNode = new TreeNode( selectItem,3,3 );
+				folderItemListUp( selectItem, tNode );
+			}
+			infoStr += ",絶対パス;";
+			infoStr += fi.FullName;//       
+		//	infoStr += ",ファイル名;";
+		//	infoStr += fi.Name;//ファイル名
+			infoStr += ",親ディレクトリ;";
+			infoStr += fi.Directory;//     
+	//		infoStr += ",親ディレクトリ名;";
+		//	infoStr += fi.DirectoryName;//親ディレクトリ名
+			fileinfo.Text = infoStr;
+			string fullPathName = fi.DirectoryName;//親ディレクトリ名= e.Node.FullPath;
+			textBox1.Text = fi.DirectoryName;    //I:\\ハイキュー 25 Haikyuu 25 (HD).mp4
+			textBox2.Text = fi.Name;//ファイル名= selectItem;
+
+
+		}
+
+		private void folderItemListUp(string sarchDir, TreeNode tNode)//, string sarchTyp
+		{
+			try {
+		//		Console.WriteLine( sarchDir );
+				string[] files = Directory.GetFiles( sarchDir );
+				if (files != null) {
+					foreach (string fileName in files) {
+						if (0 < fileName.IndexOf( "RECYCLE.BIN", StringComparison.OrdinalIgnoreCase )) {
+						} else {
+							string rfileName = fileName.Replace( sarchDir, "" );
+							tNode.Nodes.Add( rfileName );
+
+					//		listBox1.Items.Add( rfileName );      //ListBox1に結果を表示する
+						}
+					}
+				}
+				string[] foleres = Directory.GetDirectories( sarchDir );//
+				if (foleres != null) {
+					foreach (string folereName in foleres) {
+						if (0 < folereName.IndexOf( "RECYCLE", StringComparison.OrdinalIgnoreCase ) ||
+							0 < folereName.IndexOf( "System Vol", StringComparison.OrdinalIgnoreCase )) {
+						} else {
+							tNode.Nodes.Add( folereName );
+
+					//		listBox1.Items.Add( folereName );
+							//        makeFolderList(folereName);
+						}
+					}           //ListBox1に結果を表示する
+
+				}
+			} catch (UnauthorizedAccessException UAEx) {
+				Console.WriteLine( UAEx.Message );
+			} catch (PathTooLongException PathEx) {
+				Console.WriteLine( PathEx.Message );
+			}
+		}       //フォルダの中身をリストアップ
+
+
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string selectItem = listBox1.SelectedItem.ToString();
+			FileInfo fi = new FileInfo( selectItem );
+			String infoStr = ",Exists;";
+			infoStr += fi.Exists;
+			infoStr += ",拡張子;";
+			infoStr += fi.Extension;
+			infoStr += "作成;";
+			infoStr += fi.CreationTime;
+			infoStr += ",アクセス;";
+			infoStr += fi.LastAccessTime;
+			infoStr += ",更新;";
+			infoStr += fi.LastWriteTime;
+			if (fi.Exists) {
+				infoStr += ",ファイルサイズ;";
+				infoStr += fi.Length;
+			} else {
+				makeFolderList( selectItem );
+			}
+			infoStr += ",絶対パス;";
+			infoStr += fi.FullName;//       
+			infoStr += ",ファイル名;";
+			infoStr += fi.Name;
+			infoStr += ",親ディレクトリ;";
+			infoStr += fi.Directory;//     
+			infoStr += ",親ディレクトリ名;";
+			infoStr += fi.DirectoryName;
+			fileinfo.Text = infoStr;
+		}			//リストアイテムのクリック
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string selectDrive = comboBox1.SelectedItem.ToString();
+			listBox1.Items.Clear();
+			makeFolderList( selectDrive );
+		}           //ドライブセレクト
+
+
+		private void makeFolderList(string sarchDir)//, string sarchTyp
+		{
+			try {
+				string[] files = Directory.GetFiles( sarchDir );
+				if (files != null) {
+					foreach (string fileName in files) {
+						if (0 < fileName.IndexOf( "RECYCLE.BIN", StringComparison.OrdinalIgnoreCase )) {
+						} else {
+
+							string rfileName = fileName.Replace( sarchDir, "" );
+							listBox1.Items.Add( rfileName );      //ListBox1に結果を表示する
+						}
+					}     
+				}
+				string[] foleres = Directory.GetDirectories( sarchDir );//
+				if (foleres != null) {
+					foreach (string folereName in foleres) {
+						if (0 < folereName.IndexOf( "RECYCLE", StringComparison.OrdinalIgnoreCase ) ||
+							0 < folereName.IndexOf( "System Vol", StringComparison.OrdinalIgnoreCase )
+							) { } else {
+							listBox1.Items.Add( folereName );
+							 //        makeFolderList(folereName);
+						}
+					}           //ListBox1に結果を表示する
+
+				}
+			} catch (UnauthorizedAccessException UAEx) {
+				Console.WriteLine( UAEx.Message );
+			} catch (PathTooLongException PathEx) {
+				Console.WriteLine( PathEx.Message );
+			}
+
+		}       //ファイルリストアップ
+
+		private void makeFileList(string sarchDir, string sarchType)
+		{
+			string[] files = Directory.GetFiles( "c:\\" );
+			foreach (string fileName in files) {
+				listBox1.Items.Add( fileName );
+			}           //ListBox1に結果を表示する
+
+			//     System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(sarchDir);
+			//     System.IO.FileInfo[] files =di.GetFiles(sarchType, System.IO.SearchOption.AllDirectories);
+			//        foreach (System.IO.FileInfo f in files)
+			//       {
+			//           listBox1.Items.Add(f.FullName);
+			//       }           //ListBox1に結果を表示する
+
+			//以下2行でも同様      https://dobon.net/vb/dotnet/file/getfiles.html
+			//            string[] files = System.IO.Directory.GetFiles( sarchDir, sarchType, System.IO.SearchOption.AllDirectories);           //"C:\test"以下のファイルをすべて取得する
+			//         listBox1.Items.AddRange(files);           //ListBox1に結果を表示する
+		}       //ファイルリストアップ
+
+		private void makeDriveList()
+		{
+			TreeNode tn;
+			foreach (DriveInfo drive in DriveInfo.GetDrives())//http://www.atmarkit.co.jp/fdotnet/dotnettips/557driveinfo/driveinfo.html
+			{
+				string driveNames = drive.Name; // ドライブ名
+				if (drive.IsReady) { // ドライブの準備はOK？
+					comboBox1.Items.Add( driveNames ); //comboBoxに結果を表示する
+													   //         Console.WriteLine("\t{0}\t{1}\t{2}",
+													   //           drive.DriveFormat,  // フォーマット
+													   //           drive.DriveType,    // 種類
+													   //           drive.VolumeLabel); // ボリュームラベル
+						tn = new TreeNode( driveNames, 0, 0 );//ノードにドライブアイコンを設定
+						treeView1.Nodes.Add( tn );//親ノードにドライブを設定
+						folderItemListUp( driveNames, tn );
+				}
+				//       else
+				//      {
+				//          Console.WriteLine("\tNot Ready");
+				//      }
+			}
+			//    string selectDrive = @"C:\";
+			comboBox1.SelectedIndex = 3;
+
+		}//使用可能なドライブリスト取得
+
+		////時計表示////////////////////////////////////////////////////////////////////
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			SetDisplayTime();
+		}
+
+		private void SetDisplayTime()
+		{
+			timeNow.Text = DateTime.Now.ToString( "HH時mm分 ss秒" );
+		}
+	}
 }
